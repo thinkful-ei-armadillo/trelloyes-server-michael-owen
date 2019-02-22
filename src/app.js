930f25050c2cf9583f05045f73d42889c9883ab2
owencyr <cyroc@dukes.jmu.edu>
2019-02-22 12:20:14 -0500

    added basic get request and authentication

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const winston = require('winston');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
@@ -8,14 +9,51 @@ const { NODE_ENV } = require('./config');
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+//set up winston
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: 'info.log' })]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
 
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
 });
+
+const cards = [
+  {
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one'
+  }
+];
+const lists = [
+  {
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+  }
+];
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
@@ -26,6 +64,31 @@ app.use(function errorHandler(error, req, res, next) {
     response = { message: error.message, error };
   }
   res.status(500).json(response);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/card', (req, res) => {
+  res.json(cards);
+});
+
+app.get('/list', (req, res) => {
+  res.json(lists);
+});
+
+app.get('/card/:id', (req, res) => {
+  const { id } = req.params;
+  const card = cards.find(c => c.id == id);
+
+  // make sure we found a card
+  if (!card) {
+    logger.error(`Card with id ${id} not found.`);
+    return res.status(404).send('Card Not Found');
+  }
+
+  res.json(card);
 });
 
 module.exports = app;
